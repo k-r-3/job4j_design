832614733b8dc4a44cf24c7516cc14b332a18ff1
k-r-3 <rke19922802@gmail.com>
2021-03-10 23:26:40 +0500

    4. JAXB. Преобразование XML в POJO. [#469879]

--- src/main/java/ru/job4j/serialization/xml/InnerObject.java
+++ src/main/java/ru/job4j/serialization/xml/InnerObject.java
@@ -1,9 +1,24 @@
 package ru.job4j.serialization.xml;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlElement
 public class InnerObject {
-    private final int someNumber;
+    @XmlAttribute
+    private int someNumber;
+
+    public InnerObject() { }
 
     public InnerObject(int someNumber) {
         this.someNumber = someNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "InnerObject{"
+                + "someNumber="
+                + someNumber + '}';
     }
 }
--- src/main/java/ru/job4j/serialization/xml/MainClass.java
+++ src/main/java/ru/job4j/serialization/xml/MainClass.java
@@ -1,13 +1,36 @@
 package ru.job4j.serialization.xml;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@XmlRootElement(name = "MainClass")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class MainClass {
-    private final boolean tumbler;
-    private final int amount;
-    private final String name;
-    private final InnerObject in;
-    private final String[] array;
+    public static final Logger LOG = LoggerFactory.getLogger(MainClass.class.getName());
+
+    @XmlAttribute
+    private boolean tumbler;
+    @XmlAttribute
+    private int amount;
+    @XmlAttribute
+    private String name;
+    private InnerObject in;
+    @XmlElementWrapper(name = "types")
+    @XmlElement(name = "type")
+    private String[] array;
+
+    public MainClass() {
+    }
 
     public MainClass(boolean tumbler, int amount, String name, InnerObject in, String... array) {
         this.tumbler = tumbler;
@@ -21,15 +44,36 @@ public class MainClass {
     public String toString() {
         return "MainClass{"
                 + "tumbler=" + tumbler
-                + ", amount=" + amount
-                + ", name='" + name + '\''
-                + ", in=" + in
-                + ", array=" + Arrays.toString(array)
+                + ",\n amount=" + amount
+                + ",\n name='" + name + '\''
+                + ",\n in=" + in
+                + ",\n array=" + Arrays.toString(array)
                 + '}';
     }
 
     public static void main(String[] args) {
         final MainClass mainClass = new MainClass(false, 123,
-                "main class", new InnerObject(100), "First word", "Second word");
+                "main class", new InnerObject(100),
+                "First word", "Second word");
+        try {
+            JAXBContext context = JAXBContext.newInstance(MainClass.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            String xml = "";
+            try (StringWriter writer = new StringWriter()) {
+                marshaller.marshal(mainClass, writer);
+                xml = writer.getBuffer().toString();
+                System.out.println(xml);
+                Unmarshaller unmarshaller = context.createUnmarshaller();
+                try (StringReader reader = new StringReader(xml)) {
+                    MainClass deserial = (MainClass) unmarshaller.unmarshal(reader);
+                    System.out.println(deserial);
+                }
+            } catch (IOException e) {
+                LOG.error("write-read section exception", e);
+            }
+        } catch (JAXBException e) {
+            LOG.error("JAXB exception", e);
+        }
     }
 }
