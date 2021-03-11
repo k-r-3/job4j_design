a01eda564aa12a7cecc792fb19a94a07ad15c9b0
k-r-3 <rke19922802@gmail.com>
2021-03-11 23:47:26 +0500

    4.1. Сканирование файловой системы. [#469861]

--- src/test/java/ru/job4j/io/SearchTest.java
+++ src/test/java/ru/job4j/io/SearchTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
-
+import org.junit.rules.TemporaryFolder;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -10,17 +12,26 @@ import static org.junit.Assert.*;
 
 public class SearchTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenSearchJava() throws IOException {
-        Path root = Paths.get(
-                "./src/main/java/ru/job4j/collection/");
-        assertEquals(Search.search(root, ".*java").size(), 10);
+        Path rootDir = folder.newFolder().toPath();
+        Path subDir = Files.createDirectory(Paths.get(rootDir.toAbsolutePath() + "/tempdir"));
+        Path mdFile = Files.createTempFile(rootDir, null, ".md");
+        Path javaFileOuter = Files.createTempFile(rootDir, null, ".java");
+        Path javaFileInner = Files.createTempFile(subDir, null, ".java");
+        assertEquals(Search.search(rootDir, ".*java").size(), 2);
     }
 
     @Test
     public void whenSearchReadme() throws IOException {
-        Path root = Paths.get("c:/projects/job4j_design/");
-        assertEquals(Search.search(root, ".*md").size(), 1);
+        Path rootDir = folder.newFolder().toPath();
+        Path subDir = Files.createDirectory(Paths.get(rootDir.toAbsolutePath() + "/tempdir"));
+        Path mdFile = Files.createTempFile(rootDir, null, ".md");
+        Path javaFile = Files.createTempFile(subDir, null, ".java");
+        assertEquals(Search.search(rootDir, ".*md").size(), 1);
     }
 
 }
