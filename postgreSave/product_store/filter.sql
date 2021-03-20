94ff2d1b67e51aaff9dae93ab7974eef418ad42b
k-r-3 <rke19922802@gmail.com>
2021-03-21 01:09:12 +0500

    Фильтры [#469798]

---
+++ postgreSave/product_store/filter.sql
@@ -0,0 +1,23 @@
+select t.name, p.name from type as t join product p on t.id = p.type_id;
+
+select count(t.name) from product p join type t on p.type_id = t.id
+where t.name = 'сыр';
+
+select p.name from product p where upper(p.name) like upper('%мороженное%');
+
+select * from product where product.expired_date > make_date(2021, 3, 20) + interval '1 month';
+
+select * from product order by product.price desc 
+limit 1;
+
+select t.name, count(p.type_id) from type t join product p on t.id = p.type_id
+group by t.name;
+
+select * from product p join type t on t.id = p.type_id
+where t.name = 'сыр' or t.name = 'молочный продукт';
+
+select count(p.name), p.name from product p
+group by p.name
+having count(p.name) < 10;
+
+select t.name, p.name, p.price, p.expired_date from type t join product p on t.id = p.type_id
