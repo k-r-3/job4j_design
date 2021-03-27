01a0653ec5a2ac9287ab0dd3182615a7251d937c
k-r-3 <rke19922802@gmail.com>
2021-03-27 16:04:26 +0500

    Хранилище машин [#469801]

--- postgreSave/auto_parts_store/insert.sql
+++ postgreSave/auto_parts_store/insert.sql
@@ -1,14 +1,17 @@
 insert into body(name) values('Sedan');
 insert into body(name) values('Hatchback');
 insert into body(name) values('Estate wagon');
+insert into body(name) values('Coupe');
 
 insert into engine(displacement) values(1.2);
 insert into engine(displacement) values(1.5);
 insert into engine(displacement) values(2.0);
+insert into engine(displacement) values(1.0);
 
 insert into transmission(name) values('Manual');
 insert into transmission(name) values('Automatic');
 insert into transmission(name) values('Variable');
+insert into transmission(name) values('Dual-Clutch');
 
 insert into car(name, body_id, engine_id, transmission_id) values('BMW 5', 1, 2, 2);
 insert into car(name, body_id, engine_id, transmission_id) values('BMW X1', 2, 3, 1);
--- postgreSave/auto_parts_store/select.sql
+++ postgreSave/auto_parts_store/select.sql
@@ -3,8 +3,11 @@ left join body b on c.body_id = b.id
 left join engine e on c.engine_id = e.id
 left join transmission t on c.transmission_id = t.id;
 
-select * from car c left join body b on c.body_id <> b.id;
+select e.displacement from engine e left join car c on c.engine_id = e.id
+where c.id is null;
 
-select * from car c left join engine e on c.engine_id <> e.id;
+select b.name from body b left join car c on c.body_id = b.id
+where c.id is null;
 
-select * from car c left join transmission t on c.transmission_id <> t.id
+select t.name from transmission t left join car c on c.transmission_id = t.id
+where c.id is null
