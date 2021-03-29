141ef56fce218d89b58de9ef99b98d7c22a695ab
k-r-3 <rke19922802@gmail.com>
2021-03-29 12:07:41 +0500

    0.1. Statement [#469769]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -2,6 +2,7 @@ package ru.job4j.jdbc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -22,69 +23,49 @@ public class TableEditor implements AutoCloseable {
     private void initConnection() throws SQLException, ClassNotFoundException {
         try (InputStream in = TableEditor.class.getClassLoader()
                 .getResourceAsStream("app.properties")) {
-         properties.load(in);
-         Class.forName(properties.getProperty("driver_class"));
-         connection = DriverManager.getConnection(
-                 properties.getProperty("url"),
-                 properties.getProperty("username"),
-                 properties.getProperty("password"));
+            properties.load(in);
+            Class.forName(properties.getProperty("driver_class"));
+            connection = DriverManager.getConnection(
+                    properties.getProperty("url"),
+                    properties.getProperty("username"),
+                    properties.getProperty("password"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void createTable(String tableName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "create table "
-                            + "%s(%s, %s);",
-                    tableName,
-                    "id serial primary key",
-                    "name varchar(255)"
-            );
-            statement.execute(sql);
-        } catch (SQLException e) {
-            LOG.error("create table exception", e);
-        }
+        String sql = String.format(
+                "create table "
+                        + "%s(%s, %s);",
+                tableName,
+                "id serial primary key",
+                "name varchar(255)"
+        );
+        executeIt("create", sql);
     }
 
     public void dropTable(String tableName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = "drop table " + tableName;
-            statement.execute(sql);
-        } catch (SQLException e) {
-            LOG.error("drop table exception", e);
-        }
+        String sql = "drop table " + tableName;
+        executeIt("drop table", sql);
     }
 
     public void addColumn(String tableName, String columnName, String type) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s add %s %s", tableName,
-                    columnName, type);
-            statement.execute(sql);
-        } catch (SQLException e) {
-            LOG.error("add column exception", e);
-        }
+        String sql = String.format("alter table %s add %s %s", tableName,
+                columnName, type);
+        executeIt("add column", sql);
     }
 
     public void dropColumn(String tableName, String columnName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s drop column %s",
-                    tableName, columnName);
-            statement.execute(sql);
-        } catch (SQLException e) {
-            LOG.error("add column exception", e);
-        }
+        String sql = String.format("alter table %s drop column %s",
+                tableName, columnName);
+        executeIt("drop column", sql);
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format("alter table %s rename column %s to %s", tableName,
-                    columnName, newColumnName);
-            statement.execute(sql);
-        } catch (SQLException e) {
-            LOG.error("add column exception", e);
-        }
+        String sql = String.format("alter table %s rename column %s to %s", tableName,
+                columnName, newColumnName);
+        executeIt("rename table", sql);
     }
 
     public String getScheme(String tableName) throws SQLException {
@@ -105,6 +86,14 @@ public class TableEditor implements AutoCloseable {
     public void close() throws Exception {
         if (connection != null) {
             connection.close();
+        }
+    }
+
+    private void executeIt(String query, String methodName) {
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+        } catch (SQLException e) {
+            LOG.error(String.format("%s exception", methodName), e);
         }
     }
 }
