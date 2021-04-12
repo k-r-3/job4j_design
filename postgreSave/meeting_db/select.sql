ede564e41f9208a157c60931ae13da9c57cfb4b3
k-r-3 <rke19922802@gmail.com>
2021-04-12 13:57:35 +0500

    SQL [#224183]

--- postgreSave/meeting_db/select.sql
+++ postgreSave/meeting_db/select.sql
@@ -3,7 +3,8 @@ left join users u on u.id = m_u.user_id
 where m_u.status_id = 1
 group by m.name;
 
-select m.name from meeting m left join meetings_users m_u on m.id = m_u.meeting_id
-where m_u.status_id = 2
-group by m.name
-having count(m_u.status_id) = (select count(u.name) from users u)
+with data as (select m.name n from meeting m join meetings_users m_u on m.id = m_u.meeting_id
+where m_u.status_id = 1)
+select m.name from meeting m left join data d on m.name = d.n 
+where d.n is null
+
