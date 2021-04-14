a1404d95c77a4a1a28699bc41d9f8882837ed7df
k-r-3 <rke19922802@gmail.com>
2021-04-15 00:36:00 +0500

    fix

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -10,17 +10,6 @@ import static org.junit.Assert.assertThat;
 public class ConfigTest {
 
     @Test
-    public void whenPairWithoutComment() {
-        String path = "./app.properties";
-        Config config = new Config(path);
-        config.load();
-        assertThat(
-                config.value("hibernate.connection.url"),
-                is("jdbc:postgresql://127.0.0.1:5432/idea_db")
-        );
-    }
-
-    @Test
     public void whenPairWithComment() {
         String path = "./app.properties";
         Config config = new Config(path);
