ea4e971a1b52edc5a8d60a695d3d762d6edfd4ff
k-r-3 <rke19922802@gmail.com>
2021-04-15 14:33:41 +0500

    Реализации кеша на SoftReference [#469926]

---
+++ src/main/java/ru/job4j/gc/ref/cash/CashClient.java
@@ -0,0 +1,10 @@
+package ru.job4j.gc.ref.cash;
+
+public class CashClient {
+    public static void main(String[] args) {
+        String key = "GCTest.txt";
+        MyCash cash = new SystemCash();
+        String value = cash.getValue(key);
+        System.out.println(value);
+    }
+}
