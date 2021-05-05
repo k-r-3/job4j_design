761bfc313994ff7e2e200e3d5d2b4d64c298e230
k-r-3 <rke19922802@gmail.com>
2021-05-05 10:40:24 +0500

    2. Парковка машин [#469910]

--- src/main/java/ru/job4j/ood/lsp/parking/ParkingService.java
+++ src/main/java/ru/job4j/ood/lsp/parking/ParkingService.java
@@ -1,10 +1,9 @@
 package ru.job4j.ood.lsp.parking;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ParkingService {
-    private List<Parking> parking = new ArrayList<>();
+    private List<Parking> parking;
 
     public ParkingService(List<Parking> parking) {
         this.parking = parking;
